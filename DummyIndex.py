75483c3d75a957fd0955da427d373273ee1f4953
Jarl Lindrud <jarl.lindrud@microfocus.com>
2023-05-19 13:27:47 +1000

    Add dummy Python script to generate an HTML file.

---
+++ DummyIndex.py
@@ -0,0 +1,15 @@
+
+# Write out a dummy HTML file.
+
+html = '''\
+<html>
+<head>
+</head>
+<body>
+Brought to you courtesy of DummyIndex.py!
+</body>
+</html>
+'''
+
+with open('index.html', 'w') as fout:
+    fout.write(html)
